c17de20d57830c602d608c97a464ef466dc2b15c
AlexTatis <alejandrocampoh@gmail.cpm>
2023-10-19 16:47:03 +0200

    First commit

---
+++ components/ProductCard.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { Stripe } from 'stripe';
+
+const props = defineProps<{
+    product: Stripe.Product
+}>()
+
+</script>
+
+<template>
+    <div class="border rounded-lg overflow-hidden grid grid-cols-2">
+        <!--PRODUCT IMAGE-->
+        <img :src="$props.product.images[0]" class="h-48 rounded-md object-scale-down">
+        
+        <!--PRODUCT DATA-->
+        <div class="">
+            <p class="text-center text-lg font-bold">{{ $props.product.name }}</p>
+            <p class="text-center" v-if="$props.product.default_price">
+                {{ (parseInt(($props.product.default_price as Stripe.Price).unit_amount_decimal ?? "") / 100).toFixed(2) }}
+                €
+            </p>
+        </div>
+
+    </div>
+</template>
---
+++ components/ProductCardGroup.server.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { Stripe } from 'stripe'
+
+const stripe = new Stripe("sk_test_51Nv25kDdZ8symvlPfA4J8guLW0QJXM2va3xZd5c0DCIDUBgJqTMriJA9n3hWXDHbX5DoYo0YiTC1Yc4YR2NO917A00hKLcWJrt", {
+    apiVersion: '2023-10-16'
+})
+
+const products = await stripe.products.list({
+    active: true,
+    expand: ["data.default_price"]
+})
+
+</script>
+
+<template>
+    <div class="grid grid-cols-3 gap-2">
+        <ProductCard v-for="product in products.data" :product="product"></ProductCard>
+    </div>
+</template>
